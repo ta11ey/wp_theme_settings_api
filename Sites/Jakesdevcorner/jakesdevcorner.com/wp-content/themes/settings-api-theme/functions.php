e134ee45491a390f0e24d686cb67f6c09441a6d2
Jacob Talley <jacob.talley@hotmail.com>
2017-04-10 13:19:02 -0600

    added options to themes appearence tab

--- Sites/Jakesdevcorner/jakesdevcorner.com/wp-content/themes/settings-api-theme/functions.php
+++ Sites/Jakesdevcorner/jakesdevcorner.com/wp-content/themes/settings-api-theme/functions.php
@@ -10,9 +10,9 @@
  * This function is registered with the 'admin_init' hook.
  */
  add_action('admin_init', 'sandbox_initialize_theme_options');
- add_action('admin_menu', 'sandbox_create_menu_page');
- add_action('admin_menu', 'sandbox_plugin_menu');
  add_action('admin_menu', 'sandbox_theme_menu');
+
+
 
  function sandbox_theme_menu () {
    add_theme_page(
@@ -20,120 +20,84 @@
      'Sandbox Theme Custom',
      'administrator',
      'sandbox_theme',
-     'sandbox_theme_page'
+     'sandbox_theme_display'
    );
  }
 
- function sandbox_plugin_menu() {
-   add_plugins_page(
-     'My Custom Plugin',
-     'Sandbox Plugin',
-     'administrator',
-     'sandbox_plugin',
-     'sandbox_plugin_page'
-   );
- }
 
- function sandbox_theme_page() {
-   echo 'I\'m a theme page bitch';
- }
+ function sandbox_theme_display() {
+   ?>
+    <!-- Create a header in the default WordPress 'wrap' container -->
+    <div class="wrap">
 
- function sandbox_plugin_page() {
-   echo 'sup I\'m a plugin page bitch';
- }
- function sandbox_create_menu_page () {
-   add_menu_page(
-     'Sandbox Options',
-     'Sandbox',
-     'administrator',
-     'sandbox',
-     'sandbox_menu_page_display'
-   );
+        <!-- Add the icon to the page -->
+        <div id="icon-themes" class="icon32"></div>
+        <h2>Sandbox Theme Options</h2>
 
-   add_submenu_page(
-     'sandbox',
-     'Sub Menu Dog',
-     'menu text dog',
-     'administrator',
-     'sandbox_options',
-     'sandbox_submenu_page_display'
-   );
- }
+        <!-- Make a call to the WordPress function for rendering errors when settings are saved. -->
+        <?php settings_errors(); ?>
 
- function sandbox_submenu_page_display() {
-   // Create a header in the default WordPress 'wrap' container
-    $html = '<div class="wrap">';
-        $html .= '<h2>Sandbox Options</h2>';
-    $html .= '</div>';
+        <!-- Create the form that will be used to render our options -->
+        <form method="post" action="options.php">
+            <?php settings_fields( 'sandbox_theme_display_options' ); ?>
+            <?php do_settings_sections( 'sandbox_theme_display_options' ); ?>
+            <?php submit_button(); ?>
+        </form>
 
-    // Send the markup to the browser
-    echo $html;
- }
+    </div><!-- /.wrap -->
+<?php
 
- function sandbox_menu_page_display() {
-   $html = '<div class="wrap" >';
-    $html .= '<h2>Sandbox</h2>';
-   $html .= '</div>';
-   echo $html;
  }
 
  function sandbox_initialize_theme_options() {
+   if (false == get_option('sandbox_theme_display_options')) {
+     add_option('sandbox_theme_display_options');
+   }
+
+
    add_settings_section(
       'general_settings_section',
-      'Sandbox Options',
+      'Display Options',
       'sandbox_general_options_callback',
-      'general'
+      'sandbox_theme_display_options'
     );
 
-    /**
-    * setting to toggle visibility of elements on page
-    */
-  add_settings_field(
-    'show_header',
-    'Header',
-    'sandbox_toggle_header_callback',
-    'general',
-    'general_settings_section',
-    array(
-      'Activate this setting to display the header.'
-    )
-  );
+    add_settings_field(
+      'show_header',                      // ID used to identify the field throughout the theme
+      'Header',                           // The label to the left of the option interface element
+      'sandbox_toggle_header_callback',   // The name of the function responsible for rendering the option interface
+      'sandbox_theme_display_options',    // The page on which this option will be displayed
+      'general_settings_section',         // The name of the section to which this field belongs
+      array(                              // The array of arguments to pass to the callback. In this case, just a description.
+            'Activate this setting to display the header.'
+      )
+    );
 
   add_settings_field(
-    'show_content',
-    'Content',
-    'sandbox_toggle_content_callback',
-    'general',
-    'general_settings_section',
-    array(
-      'Activate this setting to display the content.'
-    )
+      'show_content',
+      'Content',
+      'sandbox_toggle_content_callback',
+      'sandbox_theme_display_options',
+      'general_settings_section',
+      array(
+          'Activate this setting to display the content.'
+      )
   );
 
   add_settings_field(
-    'show_footer',
-    'Footer',
-    'sandbox_toggle_footer_callback',
-    'general',
-    'general_settings_section',
-    array(
-      'Activate this setting to display the footer.'
-    )
+      'show_footer',
+      'Footer',
+      'sandbox_toggle_footer_callback',
+      'sandbox_theme_display_options',
+      'general_settings_section',
+      array(
+          'Activate this setting to display the footer.'
+      )
   );
 
   register_setting(
-    'general',
-    'show_header'
-  );
-
-  register_setting(
-    'general',
-    'show_content'
-  );
-
-  register_setting(
-    'general',
-    'show_footer'
+      'sandbox_theme_display_options',
+      'sandbox_theme_display_options'
   );
  };// end sandbox_initialize_theme_options
 
@@ -143,8 +107,10 @@
 
 
  function sandbox_toggle_header_callback($args) {
-   $html = '<input type="checkbox" id="show_header" name="show_header" value="1" '
-    . checked(1, get_option('show_header'), false)
+   $options = get_option('sandbox_theme_display_options');
+
+   $html = '<input type="checkbox" id="show_header" name="sandbox_theme_display_options[show_header]" value="1" '
+    . checked(1, $options['show_header'], false)
     . '/>';
 
     $html .= '<label for="show_header">'
@@ -154,8 +120,10 @@
  }
 
  function sandbox_toggle_content_callback($args) {
-   $html = '<input type="checkbox" id="show_content" name="show_content" value="1" '
-    . checked(1, get_option('show_content'), false)
+   $options = get_option('sandbox_theme_display_options');
+
+   $html = '<input type="checkbox" id="show_content" name="sandbox_theme_display_options[show_content]" value="1" '
+    . checked(1, $options['show_content'], false)
     . '/>';
 
     $html .= '<label for="show_content">'
@@ -165,8 +133,10 @@
  }
 
  function sandbox_toggle_footer_callback($args) {
-   $html = '<input type="checkbox" id="show_footer" name="show_footer" value="1" '
-    . checked(1, get_option('show_footer'), false)
+   $options = get_option('sandbox_theme_display_options');
+
+   $html = '<input type="checkbox" id="show_footer" name="sandbox_theme_display_options[show_footer]" value="1" '
+    . checked(1, $options['show_footer'], false)
     . '/>';
 
     $html .= '<label for="show_footer">'
@@ -176,4 +146,8 @@
  }
 
 
+
+/**
+* initializes themes social options by registering the Sections, Fields, and settings
+*/
 ?>
--- Sites/Jakesdevcorner/jakesdevcorner.com/wp-content/themes/settings-api-theme/index.php
+++ Sites/Jakesdevcorner/jakesdevcorner.com/wp-content/themes/settings-api-theme/index.php
@@ -4,19 +4,19 @@
         <title>The Complete Guide To The Settings API | Sandbox Theme</title>
     </head>
     <body>
-        <?php  if( get_option('show_header')) {?>
+        <?php  if( get_option('sandbox_theme_display_options')['show_header']) {?>
           <div id="header">
               <h1>Sandbox Header</h1>
           </div><!-- /#header -->
         <?php } ?>
 
-        <?php  if( get_option('show_content')) {?>
+        <?php  if( get_option('sandbox_theme_display_options')['show_content']) {?>
           <div id="content">
               <p>This is theme content.</p>
           </div><!-- /#content -->
         <?php } ?>
 
-        <?php  if( get_option('show_footer')) {?>
+        <?php  if( get_option('sandbox_theme_display_options')['show_footer']) {?>
           <div id="footer">
               <p>&copy; <?php echo date('Y'); ?> All Rights Reserved.</p>
           </div><!-- /#footer -->
